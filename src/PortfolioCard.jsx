21b2049f6d6106fac307b222c19621eb0b5ddb9f
Mishael Edegwa <mishaeledegwa@gmail.com>
2026-07-19 14:45:16 +0100

    x

--- src/PortfolioCard.jsx
+++ src/PortfolioCard.jsx
@@ -1,47 +1,58 @@
 import { useId } from 'react'
 
-const CARD_PATH =
-  'M15 15 C100 10 220 13 305 11 L305 392 C220 398 100 390 15 395 L15 15 Z'
-
 function PortfolioCard({ title, image }) {
   const clipId = useId()
 
   return (
-    <div style={{ position: 'relative', width: '100%', height: '200px' }}>
-      <svg
-        width="100%"
-        height="100%"
-        viewBox="0 0 320 400"
-        preserveAspectRatio="xMidYMid slice"
-        style={{ position: 'absolute', top: '10px', left: 0 }}
-      >
-        <path d={CARD_PATH} fill="#000000" />
-      </svg>
-
-      <svg width="100%" height="100%" viewBox="0 0 320 400" preserveAspectRatio="xMidYMid slice" style={{ position: 'absolute', top: 0, left: 0 }}>
-        <defs>
-          <clipPath id={clipId}>
-            <path d={CARD_PATH} />
-          </clipPath>
-        </defs>
-        <image href={image} x="0" y="0" width="100%" height="100%" preserveAspectRatio="xMidYMid slice" clipPath={`url(#${clipId})`} />
-        <path d={CARD_PATH} fill="rgba(0,0,0,0.7)" />
-      </svg>
-
-      <div
-        style={{
-          position: 'relative',
-          height: '100%',
-          display: 'flex',
-          alignItems: 'flex-end',
-          justifyContent: 'center',
-          padding: '30px 25px',
-          boxSizing: 'border-box',
-        }}
-      >
-        <h3 style={{ fontFamily: 'Skreeble', color: '#ffffff', fontWeight: 'normal', textAlign: 'center', fontSize: '32px', margin: 0, lineHeight: 1.1 }}>
-          {title}
-        </h3>
+    <div style={{ position: 'relative', width: '50%', height: 'auto' }}>
+      {/* Black drop shadow layer */}
+      <div style={{ position: 'absolute', top: '8px', left: '0', width: '100%', height: '200px', backgroundColor: '#000000', zIndex: 0 }} />
+      
+      {/* Main card */}
+      <div style={{ position: 'relative', width: '100%', height: '200px', overflow: 'hidden', zIndex: 1 }}>
+        {/* Background image */}
+        <div
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundImage: `url(${image})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+          }}
+        />
+        
+        {/* Dark overlay */}
+        <div
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'rgba(0,0,0,0.7)',
+          }}
+        />
+        
+        {/* Title text */}
+        <div
+          style={{
+            position: 'relative',
+            height: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            padding: '30px 25px',
+            boxSizing: 'border-box',
+          }}
+        >
+          <h3 style={{ fontFamily: 'Skreeble', color: '#ffffff', fontWeight: 'normal', textAlign: 'center', fontSize: '32px', margin: 0, lineHeight: 1.1 }}>
+            {title}
+          </h3>
+        </div>
       </div>
     </div>
   )
